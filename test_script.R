6fa4c294dce056cafc1c85d27cea2cf92e379a16
Jacopo Lenkowicz <jacopo.lenkowicz@gmail.com>
2019-10-31 12:38:00 +0100

    fixed names

---
+++ test_script.R
@@ -0,0 +1,21 @@
+library(devtools)
+install_github("kbolab/moddicom")
+
+#Use case 1: Extract features from a single patient DICOM series
+
+library(moddicom)
+obj.geolet <- geoLet()
+obj.geolet$openDICOMFolder(pathToOpen= "./test/Images/PAT1")
+obj.geolet$getROIList()
+roi <- "GTV-1"
+roiVoxels <- obj.geolet$getROIVoxels(Structure = roi)
+GTV_voxelcube <- roiVoxels$masked.images$voxelCube
+image(GTV_voxelcube[,,6], col = grey.colors(256))
+features <- computeFeatures.geoLet(obj.geoLet = obj.geolet, ROIName = roi, feature.family=c("stat","morph","glcm","rlm","szm"))
+
+#Use case 2: Extract features from multiple patients DICOM series
+
+library(moddicom)
+path <- "./test/Images"
+roi <- "GTV-1"
+features <- f.extractor.sing.par(path = path, ROIName = roi, feature.family=c("stat","morph","glcm","rlm","szm"))
