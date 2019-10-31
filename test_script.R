b3ecc841adfb6080fcc058b9d94d2501c9b5750b
Jacopo Lenkowicz <jacopo.lenkowicz@gmail.com>
2019-10-31 16:05:17 +0100

    .

--- test_script.R
+++ test_script.R
@@ -11,7 +11,7 @@ roi <- "GTV-1"
 roiVoxels <- obj.geolet$getROIVoxels(Structure = roi)
 GTV_voxelcube <- roiVoxels$masked.images$voxelCube
 image(GTV_voxelcube[,,6], col = grey.colors(256))
-features <- computeFeatures.geoLet(obj.geoLet = obj.geolet, ROIName = roi, feature.family=c("stat","morph","glcm","rlm","szm"))
+features <- computeFeatures.geoLet(obj.geoLet = obj.geolet, ROIName = roi, feature.family=c("stat","morph","glcm","rlm","szm"),discretize = "bin.number")
 
 #Use case 2: Extract features from multiple patients DICOM series
 
@@ -19,3 +19,14 @@ library(moddicom)
 path <- "./test/Images"
 roi <- "GTV-1"
 features <- f.extractor.sing.par(path = path, ROIName = roi, feature.family=c("stat","morph","glcm","rlm","szm"))
+
+
+#Use case 3: Extract features from multiple patients DICOM series with LoG filter
+
+library(moddicom)
+path <- "./test/Images"
+roi <- "GTV-1"
+features <- f.extractor.pluri.LoG.par(path = path, ROIName = roi, from.sigma = 0.6, to.sigma = 1.0, def.by =  0.2, feature.family=c("stat","morph","glcm","rlm","szm"))
+
+
+
